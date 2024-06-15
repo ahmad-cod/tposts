dda3f84f12b6d70d4ada72077ba38fec0f8dd0dc
ahmad-cod <ahmadadewale800@gmail.com>
2024-06-15 06:26:17 +0100

    fix: secrets for github workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:socialmedia_demo/auth/auth.dart';
 import 'package:socialmedia_demo/auth/login_or_register.dart';
 import 'package:socialmedia_demo/firebase_options.dart';
@@ -12,7 +12,7 @@ import 'package:socialmedia_demo/theme/light_mode.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
